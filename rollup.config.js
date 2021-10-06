fa8c8f6b365fde606b1c3b6264c4af7dfe1f1f35
Jacano <641283231@qq.com>
2021-10-07 06:55:40 +0800

    build: package to min

--- rollup.config.js
+++ rollup.config.js
@@ -1,32 +1,65 @@
 import { defineConfig } from 'rollup'
 import typescript from 'rollup-plugin-typescript2'
 import { terser } from 'rollup-plugin-terser'
+import pkg from './package.json'
 
-export default defineConfig({
-  input: './src/index.ts',
-  output: [
-    {
-      file: 'dist/index.js',
-      format: 'umd',
-      name: 'ha',
-    },
-    {
-      file: 'es/index.js',
-      format: 'es',
-    },
-  ],
-  plugins: [
-    typescript({
-      tsconfig: 'tsconfig.json',
-      useTsconfigDeclarationDir: true,
-    }),
-    terser({
-      compress: {
-        pure_getters: true,
-        unsafe: true,
-        unsafe_comps: true,
-        warnings: false,
-      },
-    }),
-  ],
-})
+function createESMConfig(input, output, production) {
+  return defineConfig({
+    input,
+    output: { file: output, format: 'esm' },
+    plugins: [
+      typescript({
+        tsconfig: 'tsconfig.json',
+        useTsconfigDeclarationDir: true,
+      }),
+      production
+        ? terser({
+            compress: {
+              pure_getters: true,
+              unsafe: true,
+              unsafe_comps: true,
+              warnings: false,
+            },
+            format: {
+              comments: RegExp(`${pkg.name}`),
+            },
+          })
+        : [],
+    ],
+  })
+}
+
+function createUMDConfig(input, output, production) {
+  return defineConfig({
+    input,
+    output: { file: output, format: 'umd', name: 'af' },
+    plugins: [
+      typescript({
+        tsconfig: 'tsconfig.json',
+        useTsconfigDeclarationDir: true,
+      }),
+      production
+        ? terser({
+            compress: {
+              pure_getters: true,
+              unsafe: true,
+              unsafe_comps: true,
+              warnings: false,
+            },
+            format: {
+              comments: RegExp(`${pkg.name}`),
+            },
+          })
+        : [],
+    ],
+  })
+}
+
+export default function () {
+  return [
+    createESMConfig('src/index.ts', 'es/index.js', false),
+    createESMConfig('src/index.ts', 'es/index.min.js', true),
+    createUMDConfig('src/index.ts', 'dist/ajax-fake.js', false),
+    createUMDConfig('src/index.ts', 'dist/ajax-fake.min.js', true),
+  ]
+}
